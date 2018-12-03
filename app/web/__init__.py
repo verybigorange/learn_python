eebc9fa5e4f78f923dfffe93ef39663f4da4fb71
zc <449732828@qq.com>
2018-12-03 16:22:04 +0800

    修改目录

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -3,10 +3,6 @@ import os
 
 web = Blueprint('web', __name__, template_folder=os.path.abspath('templates'))
 
-
-
-
-
 # 注意这里要导入用到的视图模块
 from app.web import index
 from app.web import login
--- app/web/index.py
+++ app/web/index.py
@@ -1,7 +1,6 @@
 from . import web
 from flask import render_template,session,redirect
 from app.helper.auth import LoginAuth
-# from flask_login import login_required
 
 @web.route('/')
 @LoginAuth.login_required
